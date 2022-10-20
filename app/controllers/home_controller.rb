0e80897a6538563172f1674db68bb08e4bd20b5c
mustafayildiz12 <hasansabbah284@gmail.com>
2022-10-21 00:32:54 +0300

    heroku deploy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ApplicationController
+  before_action :about
+  before_action :multi
   def index
+   
   end
   def about
+    @zafer = "Zafer"
+    @get_this = 11+14
+ 
   end
+  def multi
+    @zoo = "Zafer"
+    @get_sum = 11+14
+ 
+  end
+ # helper_method :multi
+
 end
